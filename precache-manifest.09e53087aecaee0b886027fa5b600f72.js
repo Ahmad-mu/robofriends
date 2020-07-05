659c5bb704930efaa77b786ac931ed391535462c
Your Name <you@example.com>
2020-07-05 22:02:51 +0430

    Updates

---
+++ precache-manifest.09e53087aecaee0b886027fa5b600f72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4102f0f7f35fb7cf2c76e09f6df3b8df",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "384d1893aac23681d69e",
+    "url": "/robofriends/static/css/main.e23030f1.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "384d1893aac23681d69e",
+    "url": "/robofriends/static/js/main.3411f895.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "ca18221dc3b4d1fd69c49df9f2701e86",
+    "url": "/robofriends/static/media/Strawberry Shortcake NEW.ca18221d.woff"
+  }
+]);
